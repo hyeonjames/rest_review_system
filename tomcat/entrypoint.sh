9aea77580b62d761dcd54806b0eb2f2b683b84fc
김승현 <2012003909@hanyang.ac.kr>
2018-12-08 03:32:29 +0900

    cc

--- tomcat/entrypoint.sh
+++ tomcat/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "DB Server Wait"
-dockerize -wait tcp://mysql_rest_service:3306 -timeout 20s
+dockerize -wait tcp://mysql_rest_service:3306 -timeout 40s
 
 echo "TOMCAT START $CATALINA_HOME" 
 catalina.sh run
